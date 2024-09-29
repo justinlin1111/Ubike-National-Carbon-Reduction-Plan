aaa695deec041db4ce72b9970d774ef6f7132fa3
justinlin1111 <justinlin1111@gmail.com>
2024-09-29 16:44:00 +0800

    nothing

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+import random
+
 class Station:
     def __init__(self, name, total) -> None:
         self.name = name
         self.total = total
-        
+        self.cur = random.randint(0, self.total)
+        self.needed = random.randint(0, self.total)
+        self.diff = self.cur - self.needed
 
 Station1 = Station("台大男一舍前", 61)
 Station2 = Station("台大新體育館東南側", 39)
 Station3 = Station("台大總圖書館西南側", 23)
 Station4 = Station("捷運公館站(2號出口)", 77)
+
+Stations = []
+Stations.append(Station1)
+Stations.append(Station2)
+Stations.append(Station3)
+Stations.append(Station4)
 
 wage = 200
 drive_times = 4
@@ -15,3 +25,9 @@ oil_price = 3.3
 distance = 1
 drive_hourly_cost = wage + oil_price * distance * drive_times
 personal_cost = 3
+bike_per_times = 40
+
+# 無D12
+for station in Stations:
+    # 演算法
+    pass
